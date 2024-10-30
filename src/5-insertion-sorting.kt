402191698e7f54dbfe893834cbe40231c89e66f1
Leon <leo.muscle28@gmail.com>
2024-10-30 18:14:09 +0000

    Алгоритм сортування вставками - OK 100

--- src/5-insertion-sorting.kt
+++ src/5-insertion-sorting.kt
@@ -1,22 +1,31 @@
 fun main() {
     val n = readLine()!!.toInt()
-    val array = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+    val arr = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
 
-    if (array == array.sorted()) {
-        println("The array is already sorted")
-        return
-    }
+    var alreadySorted = true
 
     for (i in 1 until n) {
-        val key = array[i]
+        val key = arr[i]
         var j = i - 1
 
-        while (j >= 0 && array[j] > key) {
-            array[j + 1] = array[j]
-            j--
+        if (arr[i] < arr[i - 1]) {
+            alreadySorted = false
         }
-        array[j + 1] = key
 
-        println(array.joinToString(" "))
+        var hasChanged = false
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j]
+            j--
+            hasChanged = true
+        }
+        arr[j + 1] = key
+
+        if (hasChanged) {
+            println(arr.joinToString(" "))
+        }
+    }
+
+    if (alreadySorted) {
+        println("The array is already sorted")
     }
 }
